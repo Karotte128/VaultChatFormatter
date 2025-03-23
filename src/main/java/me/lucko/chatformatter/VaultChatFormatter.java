5500bdcae9f85199c5885e833839a702bb718078
Karotte128 <karotte128@outlook.de>
2025-03-23 19:44:43 +0100

    switch to AsyncChatEvent

--- src/main/java/me/lucko/chatformatter/VaultChatFormatter.java
+++ src/main/java/me/lucko/chatformatter/VaultChatFormatter.java
@@ -1,5 +1,7 @@
 package me.lucko.chatformatter;
 
+import io.papermc.paper.event.player.AsyncChatEvent;
+import net.kyori.adventure.text.Component;
 import net.milkbowl.vault.chat.Chat;
 
 import org.bukkit.ChatColor;
@@ -8,7 +10,6 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.server.ServiceRegisterEvent;
 import org.bukkit.event.server.ServiceUnregisterEvent;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -25,8 +26,6 @@ public class VaultChatFormatter extends JavaPlugin implements Listener {
 
     // Format placeholders
     private static final String NAME_PLACEHOLDER = "{name}";
-    private static final String DISPLAYNAME_PLACEHOLDER = "{displayname}";
-    private static final String MESSAGE_PLACEHOLDER = "{message}";
     private static final String PREFIX_PLACEHOLDER = "{prefix}";
     private static final String SUFFIX_PLACEHOLDER = "{suffix}";
 
@@ -36,7 +35,7 @@ public class VaultChatFormatter extends JavaPlugin implements Listener {
     private static final Pattern SUFFIX_PLACEHOLDER_PATTERN = Pattern.compile(SUFFIX_PLACEHOLDER, Pattern.LITERAL);
 
     /** The default format */
-    private static final String DEFAULT_FORMAT = "<" + PREFIX_PLACEHOLDER + NAME_PLACEHOLDER + SUFFIX_PLACEHOLDER + "> " + MESSAGE_PLACEHOLDER;
+    private static final String DEFAULT_FORMAT = "<" + PREFIX_PLACEHOLDER + NAME_PLACEHOLDER + SUFFIX_PLACEHOLDER + "> ";
 
     /** Pattern matching "nicer" legacy hex chat color codes - &#rrggbb */
     private static final Pattern NICER_HEX_COLOR_PATTERN = Pattern.compile("&#([0-9a-fA-F]{6})");
@@ -59,11 +58,7 @@ public class VaultChatFormatter extends JavaPlugin implements Listener {
     }
 
     private void reloadConfigValues() {
-        // Replace the {displayname} and {message} placeholders used in our format with
-        // the String.format tokens used by Bukkit.
-        this.format = colorize(getConfig().getString("format", DEFAULT_FORMAT)
-                .replace(DISPLAYNAME_PLACEHOLDER, "%1$s")
-                .replace(MESSAGE_PLACEHOLDER, "%2$s"));
+        this.format = colorize(getConfig().getString("format", DEFAULT_FORMAT));
     }
 
     private void refreshVault() {
@@ -101,24 +96,20 @@ public class VaultChatFormatter extends JavaPlugin implements Listener {
         }
     }
 
-    @EventHandler(priority = EventPriority.LOWEST)
-    public void onChatLow(AsyncPlayerChatEvent e) {
-        // Set out format on the lowest priority - allow other plugins to override or add their own parts.
-        e.setFormat(this.format);
-    }
-
-    @EventHandler(priority = EventPriority.HIGHEST)
-    public void onChatHigh(AsyncPlayerChatEvent e) {
-        // Replace our placeholders on highest - just before
-        String format = e.getFormat();
-
+    @EventHandler
+    public void onAsyncChat(AsyncChatEvent e) {
         if (this.vaultChat != null) {
-            format = replaceAll(PREFIX_PLACEHOLDER_PATTERN, format, () -> colorize(this.vaultChat.getPlayerPrefix(e.getPlayer())));
-            format = replaceAll(SUFFIX_PLACEHOLDER_PATTERN, format, () -> colorize(this.vaultChat.getPlayerSuffix(e.getPlayer())));
+            format = replaceAll(PREFIX_PLACEHOLDER_PATTERN, format, colorize(this.vaultChat.getPlayerPrefix(e.getPlayer())));
+            format = replaceAll(SUFFIX_PLACEHOLDER_PATTERN, format, colorize(this.vaultChat.getPlayerSuffix(e.getPlayer())));
         }
-        format = replaceAll(NAME_PLACEHOLDER_PATTERN, format, () -> e.getPlayer().getName());
+        format = replaceAll(NAME_PLACEHOLDER_PATTERN, format, e.getPlayer().getName());
 
-        e.setFormat(format);
+        e.renderer((source, sourceDisplayName, message, viewer) -> Component.text()
+                .append(Component.text(format))
+                .append(message)
+                .build()
+        );
+
     }
 
     /**
@@ -130,10 +121,10 @@ public class VaultChatFormatter extends JavaPlugin implements Listener {
      * @param replacement the replacement
      * @return the input string with the replacements applied
      */
-    private static String replaceAll(Pattern pattern, String input, Supplier<String> replacement) {
+    private static String replaceAll(Pattern pattern, String input, String replacement) {
         Matcher matcher = pattern.matcher(input);
         if (matcher.find()) {
-            return matcher.replaceAll(Matcher.quoteReplacement(replacement.get()));
+            return matcher.replaceAll(Matcher.quoteReplacement(replacement));
         }
         return input;
     }
@@ -151,7 +142,7 @@ public class VaultChatFormatter extends JavaPlugin implements Listener {
 
         // Convert from the '&#rrggbb' hex color format to the '&x&r&r&g&g&b&b' one used by Bukkit.
         Matcher matcher = NICER_HEX_COLOR_PATTERN.matcher(string);
-        StringBuffer sb = new StringBuffer();
+        StringBuilder sb = new StringBuilder();
 
         while (matcher.find()) {
             StringBuilder replacement = new StringBuilder(14).append("&x");
